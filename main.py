ade2adc056b9eb6ab363653ca8e3cf5417e09484
Shahzaib Raza <razas2@southernct.edu>
2026-02-17 16:39:02 -0500

    Converted code in main.py

--- main.py
+++ main.py
@@ -1,8 +1,11 @@
 """There are 3 errors in this code. Please use your own ability to find them all. Consider this a Python refresher."""
+def main():
+    total = 0
+    for i in range(5):
+        number = input("Enter a number: ")
+        total += int(number)
 
-total = 0
-for i in range(5):
-    number = input("Enter a number: ")
-    total += int(number)
+    print("The running total is: " + str(total))
 
-print("The running total is: " + str(total))
+if __name__ == "__main__":
+    main()
